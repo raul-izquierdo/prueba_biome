b48d9701c7bbc8dc16a01a43dce14c70e9832591
Raúl Izquierdo Castanedo <raul.izquierdo@gmail.com>
2025-03-13 18:39:57 +0000

    feat: start

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'fix',
+                'refac',
+                'feat',
+                'remove',
+                'deprecate',
+                'test',
+                'build',
+                'docs',
+                'style',
+                'perf',
+                'revert',
+                'ui',
+                'release',
+            ],
+        ],
+    },
+}
