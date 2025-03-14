56f6b8a343701939bb7b1c8fdf81c09a9693d613
raul-izquierdo <raul.izquierdo@gmail.com>
2025-03-14 13:03:01 +0000

    fix: remove duplicate console log statements in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,4 @@ console.log('Hello, World!')
 console.log('Hello, World!')
 console.log('Hello, World!')
 console.log('Hello, World!')
-console.log('Hello, World!')
-console.log('Hello, World!')
-console.log('2Hello, World!');
+console.log('2Hello, World!')
